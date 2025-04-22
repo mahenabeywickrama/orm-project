ef17895b73d3cf8c3d8f8a1abe4fb83cd900915e
mahenabeywickrama <mahenabeywickrema2003@gmail.com>
2025-04-22 17:37:03 +0530

    Updated Project

--- src/main/java/lk/ijse/gdse/project/theserenitymentalhealththerapycenterproject/config/FactoryConfiguration.java
+++ src/main/java/lk/ijse/gdse/project/theserenitymentalhealththerapycenterproject/config/FactoryConfiguration.java
@@ -1,23 +1,43 @@
 package lk.ijse.gdse.project.theserenitymentalhealththerapycenterproject.config;
 
+import lk.ijse.gdse.project.theserenitymentalhealththerapycenterproject.entity.*;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.io.IOException;
+import java.util.Properties;
 
 public class FactoryConfiguration {
     private static FactoryConfiguration factoryConfiguration;
     private final SessionFactory sessionFactory;
 
-    private FactoryConfiguration() {
-        Configuration config = new Configuration();
-        config.configure();
-        sessionFactory = config.buildSessionFactory();
+    private FactoryConfiguration() throws IOException {
+        Configuration configuration = new Configuration();
+        Properties properties = new Properties();
+
+        properties.load(Thread.currentThread()
+                .getContextClassLoader()
+                .getResourceAsStream("hibernate.properties"));
+
+        configuration.setProperties(properties);
+
+        configuration.addAnnotatedClass(Patient.class);
+        configuration.addAnnotatedClass(Enrollment.class);
+        configuration.addAnnotatedClass(Payment.class);
+        configuration.addAnnotatedClass(Therapist.class);
+        configuration.addAnnotatedClass(TherapyProgram.class);
+        configuration.addAnnotatedClass(TherapySession.class);
+        configuration.addAnnotatedClass(User.class);
+
+        sessionFactory = configuration.buildSessionFactory();
     }
 
-    public static FactoryConfiguration getInstance() {
-        return (factoryConfiguration == null) ?
-                factoryConfiguration =
-                        new FactoryConfiguration() : factoryConfiguration;
+    public static FactoryConfiguration getInstance() throws IOException {
+        if (factoryConfiguration == null) {
+            factoryConfiguration = new FactoryConfiguration();
+        }
+        return factoryConfiguration;
     }
 
     public Session getSession() {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,10 +1,20 @@
 module lk.ijse.gdse.project.theserenitymentalhealththerapycenterproject {
     requires javafx.controls;
     requires javafx.fxml;
+    requires lombok;
+//    requires com.jfoenix;
+//    requires net.sf.jasperreports.core;
+//    requires java.mail;
+
     requires org.hibernate.orm.core;
     requires jakarta.persistence;
-    requires static lombok;
+    requires java.naming;
 
+    opens lk.ijse.gdse.project.theserenitymentalhealththerapycenterproject.config to jakarta.persistence;
+    opens lk.ijse.gdse.project.theserenitymentalhealththerapycenterproject.entity to org.hibernate.orm.core;
+
+//    opens lk.ijse.gdse.project.ormproject.dto.tm to javafx.base;
+//    opens lk.ijse.gdse.project.ormproject.controller to javafx.fxml;
 
     opens lk.ijse.gdse.project.theserenitymentalhealththerapycenterproject to javafx.fxml;
     exports lk.ijse.gdse.project.theserenitymentalhealththerapycenterproject;
